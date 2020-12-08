a07a22ef86916534692db333232f0c295630b79f
Mihir <74041830+mihirs1797@users.noreply.github.com>
2020-12-08 22:17:03 +0530

    Update title

--- APIData.js
+++ APIData.js
@@ -1,24 +1,16 @@
 export default class ApiData{
-  API_KEY="5mX4G9St6mQSrIW4VsG2ZXkR5ATfcoQh"
+  API_KEY="uECaOmN7IAdmnG14UJMgI3kchfDO8dQz"
   URL="https://api.nytimes.com/svc/topstories/v2/"
-  URL_HOME="https://api.nytimes.com/svc/topstories/v2/home.json?api-key=5mX4G9St6mQSrIW4VsG2ZXkR5ATfcoQh"
-  URL_WORLD="https://api.nytimes.com/svc/topstories/v2/world.json?api-key=5mX4G9St6mQSrIW4VsG2ZXkR5ATfcoQh"
-  URL_POLITICS="https://api.nytimes.com/svc/topstories/v2/politics.json?api-key=5mX4G9St6mQSrIW4VsG2ZXkR5ATfcoQh"
-  URL_MAGAZINE="https://api.nytimes.com/svc/topstories/v2/magazine.json?api-key=5mX4G9St6mQSrIW4VsG2ZXkR5ATfcoQh"
-  URL_TECHNOLOGY="https://api.nytimes.com/svc/topstories/v2/technology.json?api-key=5mX4G9St6mQSrIW4VsG2ZXkR5ATfcoQh"
-  URL_SCIENCE="https://api.nytimes.com/svc/topstories/v2/science.json?api-key=5mX4G9St6mQSrIW4VsG2ZXkR5ATfcoQh"
-  URL_HEALTH="https://api.nytimes.com/svc/topstories/v2/health.json?api-key=5mX4G9St6mQSrIW4VsG2ZXkR5ATfcoQh"
-  URL_SPORTS="https://api.nytimes.com/svc/topstories/v2/sports.json?api-key=5mX4G9St6mQSrIW4VsG2ZXkR5ATfcoQh"
-  URL_ARTS="https://api.nytimes.com/svc/topstories/v2/arts.json?api-key=5mX4G9St6mQSrIW4VsG2ZXkR5ATfcoQh"
-  URL_FASHION="https://api.nytimes.com/svc/topstories/v2/fashion.json?api-key=5mX4G9St6mQSrIW4VsG2ZXkR5ATfcoQh"
-  URL_FOOD="https://api.nytimes.com/svc/topstories/v2/food.json?api-key=5mX4G9St6mQSrIW4VsG2ZXkR5ATfcoQh"
-  URL_TRAVEL="https://api.nytimes.com/svc/topstories/v2/travel.json?api-key=5mX4G9St6mQSrIW4VsG2ZXkR5ATfcoQh"
+  URL_HOME="https://api.nytimes.com/svc/topstories/v2/home.json?api-key=uECaOmN7IAdmnG14UJMgI3kchfDO8dQz"
+  URL_WORLD="https://api.nytimes.com/svc/topstories/v2/world.json?api-key=uECaOmN7IAdmnG14UJMgI3kchfDO8dQz"
+  URL_POLITICS="https://api.nytimes.com/svc/topstories/v2/politics.json?api-key=uECaOmN7IAdmnG14UJMgI3kchfDO8dQz"
+  URL_MAGAZINE="https://api.nytimes.com/svc/topstories/v2/magazine.json?api-key=uECaOmN7IAdmnG14UJMgI3kchfDO8dQz"
+  URL_TECHNOLOGY="https://api.nytimes.com/svc/topstories/v2/technology.json?api-key=uECaOmN7IAdmnG14UJMgI3kchfDO8dQz"
+  URL_SCIENCE="https://api.nytimes.com/svc/topstories/v2/science.json?api-key=uECaOmN7IAdmnG14UJMgI3kchfDO8dQz"
+  URL_HEALTH="https://api.nytimes.com/svc/topstories/v2/health.json?api-key=uECaOmN7IAdmnG14UJMgI3kchfDO8dQz"
+  URL_SPORTS="https://api.nytimes.com/svc/topstories/v2/sports.json?api-key=uECaOmN7IAdmnG14UJMgI3kchfDO8dQz"
+  URL_ARTS="https://api.nytimes.com/svc/topstories/v2/arts.json?api-key=uECaOmN7IAdmnG14UJMgI3kchfDO8dQz"
+  URL_FASHION="https://api.nytimes.com/svc/topstories/v2/fashion.json?api-key=uECaOmN7IAdmnG14UJMgI3kchfDO8dQz"
+  URL_FOOD="https://api.nytimes.com/svc/topstories/v2/food.json?api-key=uECaOmN7IAdmnG14UJMgI3kchfDO8dQz"
+  URL_TRAVEL="https://api.nytimes.com/svc/topstories/v2/travel.json?api-key=uECaOmN7IAdmnG14UJMgI3kchfDO8dQz"
 }
-
-// export default class User {
-//   constructor(name) {
-//     this.name = name;
-//   }
-
-//   job = 'eng';
-// }
